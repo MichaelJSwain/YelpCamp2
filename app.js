35f4f3c25875a62f397ecb2ac00fc689dd8596bd
MichaelJSwain <42355296+MichaelJSwain@users.noreply.github.com>
2023-07-21 06:40:59 +0200

    Add the index route that will display all campgrounds

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.render("home");
 });
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
 
 app.listen(3000, () => {
   console.log("Serving on port 3000");
