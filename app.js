127c03b51db2bb5e6d1c0794673c6b401e277218
MichaelJSwain <42355296+MichaelJSwain@users.noreply.github.com>
2023-07-21 06:55:43 +0200

    Add show route to display information about 1 campground

--- app.js
+++ app.js
@@ -21,9 +21,15 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.render("home");
 });
+// INDEX
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
+});
+// SHOW
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
 });
 
 app.listen(3000, () => {
