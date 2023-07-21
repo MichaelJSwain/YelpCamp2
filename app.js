4186c491e8319ce8c00bfb153f641853a62429aa
MichaelJSwain <42355296+MichaelJSwain@users.noreply.github.com>
2023-07-21 05:45:02 +0200

    Add campground model and connect to remote MongoDB via Mongoose

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 
+mongoose.connect(
+  "mongodb+srv://michaeljswain:qmXlCRrWYNibQUwP@cluster0.jj9wseb.mongodb.net/YelpCamp?retryWrites=true&w=majority"
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
