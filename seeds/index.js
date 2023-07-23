cf3ec5c77b52b6a6f266ee2a4e571d0354f14204
MichaelJSwain <42355296+MichaelJSwain@users.noreply.github.com>
2023-07-23 05:25:59 +0200

    Update seeds file, model and show route to display image, price and description

--- seeds/index.js
+++ seeds/index.js
@@ -18,8 +18,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const campground = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/190727",
+      price,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
     });
     await campground.save();
@@ -28,3 +33,4 @@ const seedDB = async () => {
 seedDB().then(() => {
   mongoose.connection.close();
 });
+//https://source.unsplash.com/collection/190727/1600x900
