3c5cd83442ca94075a6df55683938328a8aaeb3a
MichaelJSwain <42355296+MichaelJSwain@users.noreply.github.com>
2023-07-26 05:14:54 +0200

    Add basic err handler middleware

--- app.js
+++ app.js
@@ -40,10 +40,14 @@ app.get("/campgrounds/:id", async (req, res) => {
   res.render("campgrounds/show", { campground });
 });
 // CREATE
-app.post("/campgrounds", async (req, res) => {
-  const campground = new Campground(req.body.campground);
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`);
+app.post("/campgrounds", async (req, res, next) => {
+  try {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  } catch (e) {
+    next(e);
+  }
 });
 // EDIT
 app.get("/campgrounds/:id/edit", async (req, res) => {
@@ -63,6 +67,10 @@ app.delete("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect("/campgrounds");
+});
+
+app.use((err, req, res, next) => {
+  res.send("something went wrong");
 });
 
 app.listen(3000, () => {
