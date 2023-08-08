99ff13d2a465362c046b24b3ee0ff0d7325cd032
MichaelJSwain <42355296+MichaelJSwain@users.noreply.github.com>
2023-08-08 06:15:12 +0200

    Set up express session and session config

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -28,6 +29,18 @@ app.use(methodOverride("_method"));
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+  secret: "thisshouldbeabettersecret!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 
 app.get("/", (req, res) => {
   res.render("home");
