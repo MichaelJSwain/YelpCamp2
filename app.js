ff759e2237c5f3bfd3a562dbed19785bcd668968
MichaelJSwain <42355296+MichaelJSwain@users.noreply.github.com>
2023-08-09 06:31:43 +0200

    Add flash message partial and bootstrap alert to display flash messages

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
+const flash = require("connect-flash");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -26,8 +27,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
 app.use(express.static(path.join(__dirname, "public")));
 
 const sessionConfig = {
@@ -41,6 +40,16 @@ const sessionConfig = {
   },
 };
 app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+app.use("/campgrounds", campgrounds);
+app.use("/campgrounds/:id/reviews", reviews);
 
 app.get("/", (req, res) => {
   res.render("home");
